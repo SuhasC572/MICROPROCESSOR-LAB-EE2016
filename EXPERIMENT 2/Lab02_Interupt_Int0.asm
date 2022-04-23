dede8b2639e0f2d754cf6b9bcf25396580e9a65b
SUHAS C PRABHU <84210024+SuhasC572@users.noreply.github.com>
2022-04-24 00:54:57 +0530

    Create Lab02_Interupt_Int0.asm

---
+++ EXPERIMENT 2/Lab02_Interupt_Int0.asm
@@ -0,0 +1,78 @@
+;
+; Lab02_Interupt_Int0.asm
+;
+; Created: 27-09-2021 14:28:45
+; Author : suhas
+;
+
+#include "m8def.inc"
+
+.org 0 
+rjmp reset ; 
+
+.org 0x0000;        
+rjmp int0_ISR;
+
+.org 0x0100;
+
+reset:
+        LDI R16,0x70 ; 
+        OUT SPL,R16 ; 
+        LDI R16,0x00;
+        OUT SPH,R16;
+
+        LDI R16,0x01;
+        OUT DDRB,R16;
+
+        LDI R16,0x00;
+        OUT DDRD,R16;
+
+        IN R16, MCUCR;	
+        ORI R16, 0x00;
+        OUT MCUCR, R16;
+
+        IN R16, GICR;		Load GICR register
+        ORI R16, 0x40;
+        OUT GICR, R16;
+
+        LDI R16, 0x00;
+        OUT PORTB, R16;
+
+        SEI;
+
+ind_loop:
+    RJMP ind_loop;
+
+int1_ISR:
+    IN R16, SREG;
+    PUSH R16;
+
+    LDI R16, 0x0A;
+    MOV R0, R16;
+
+    c1:	 LDI R16, 0x01;
+    OUT PORTB, R16
+
+    LDI R16, 0xFF
+    a1:	 LDI R17, 0xFF
+    a2:	 DEC R17
+    BRNE a2
+    DEC R16
+    BRNE a1
+
+    LDI R16, 0x00
+    OUT PORTB, R16
+
+    LDI R16, 0xFF
+    b1:	 LDI R17, 0xFF
+    b2:	 DEC R17
+    BRNE b2
+    DEC R16
+    BRNE b1
+
+    DEC R0
+    BRNE c1
+    POP R16
+    OUT SREG, R16
+
+    RETI
